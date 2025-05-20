4b0144dc21befb310383e0ebabf1e46db094e565
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2025-05-20 11:01:19 +0100

    Add files via upload

--- src/cssettingsdialog.cpp
+++ src/cssettingsdialog.cpp
@@ -1,4 +1,4 @@
-/* MCP2210 Commander - Version 1.0.1 for Debian Linux
+/* MCP2210 Commander - Version 1.0.2 for Debian Linux
    Copyright (c) 2023-2025 Samuel Lourenço
 
    This program is free software: you can redistribute it and/or modify it
